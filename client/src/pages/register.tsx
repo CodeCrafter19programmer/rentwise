ccf464f7ced6ac6cf65b3b26c0463322fbb97b35
Ntale Stephen <ntalestephen@192.168.1.64>
2025-12-31 11:27:13 +0300

    Fix JSX syntax error in register.tsx - remove leftover closing bracket

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -162,7 +162,6 @@ export default function RegisterPage() {
                     </Button>
                   </form>
                 </Form>
-              )}
             </CardContent>
           </Card>
 
